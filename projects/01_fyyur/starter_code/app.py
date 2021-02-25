704cca33924ecd6bcdf22cf8f071f2f14d3475e3
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-02-26 01:57:46 +0200

    complete shows route

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -542,43 +542,28 @@ def shows():
     # displays list of shows at /shows
     # TODO: replace with real venues data.
     #       num_shows should be aggregated based on number of upcoming shows per venue.
-    data = [{
-        "venue_id": 1,
-        "venue_name": "The Musical Hop",
-        "artist_id": 4,
-        "artist_name": "Guns N Petals",
-        "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-        "start_time": "2019-05-21T21:30:00.000Z"
-    }, {
-        "venue_id": 3,
-        "venue_name": "Park Square Live Music & Coffee",
-        "artist_id": 5,
-        "artist_name": "Matt Quevedo",
-        "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-        "start_time": "2019-06-15T23:00:00.000Z"
-    }, {
-        "venue_id": 3,
-        "venue_name": "Park Square Live Music & Coffee",
-        "artist_id": 6,
-        "artist_name": "The Wild Sax Band",
-        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-        "start_time": "2035-04-01T20:00:00.000Z"
-    }, {
-        "venue_id": 3,
-        "venue_name": "Park Square Live Music & Coffee",
-        "artist_id": 6,
-        "artist_name": "The Wild Sax Band",
-        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-        "start_time": "2035-04-08T20:00:00.000Z"
-    }, {
-        "venue_id": 3,
-        "venue_name": "Park Square Live Music & Coffee",
-        "artist_id": 6,
-        "artist_name": "The Wild Sax Band",
-        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-        "start_time": "2035-04-15T20:00:00.000Z"
-    }]
-    return render_template('pages/shows.html', shows=data)
+    try:
+        query = db.session.query(Show).join(Artist).join(Venue)
+        shows = []
+        for show in query:
+            # foundArtist = Artist.query.get(show.artist_id)
+            # foundVenue = Venue.query.get(show.venue_id)
+            shows.append(
+                {"venue_id": show.venue_id,
+                 "venue_name": show.venue.name,
+                 "artist_id": show.artist_id,
+                 "artist_name": show.artist.name,
+                 "artist_image_link": show.artist.image_link,
+                 "start_time": show.start_time}
+            )
+        return render_template('pages/shows.html', shows=shows)
+    except:
+        db.session.rollback()
+        print(sys.exc_info())
+        flash("Sorry! An error occured.")
+        return redirect(url_for("index"))
+    finally:
+        db.session.close()
 
 
 @app.route('/shows/create')
@@ -592,13 +577,63 @@ def create_shows():
 def create_show_submission():
     # called to create new shows in the db, upon submitting new show listing form
     # TODO: insert form data as a new Show record in the db, instead
+    try:
+        form = ShowForm(request.form)
+        artist = form.artist_id.data
+        if not Artist.query.get(artist):
+            flash("Artist not found. Please try again")
+            return redirect(url_for('create_show_submission'))
 
-    # on successful db insert, flash success
-    flash('Show was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Show could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-    return render_template('pages/home.html')
+        venue = form.venue_id.data
+        if not Venue.query.get(venue):
+            flash("Venue not found. Please try again")
+            return redirect(url_for('create_show_submission'))
+
+        venueShowList = db.session.query(Show).filter_by(venue_id=venue).all()
+        artistShowList = db.session.query(
+            Show).filter_by(artist_id=artist).all()
+
+        start_time = form.start_time.data
+
+        if isDate(start_time):
+            d1 = datetime.strftime(
+                start_time, '%Y-%m-%d %H:%M:%S')
+            # Check if venue is available for date
+            if venueShowList:
+                for show in venueShowList:
+                    d2 = datetime.datetime.strptime(
+                        show.time, '%Y-%m-%d %H:%M:%S').date()
+                    if d2 == d1:
+                        flash("Sorry venue is already booked at date!")
+                        return redirect(url_for('create_shows'))
+            # Check if artist is available for date
+            if artistShowList:
+                for show in artistShowList:
+                    d2 = datetime.datetime.strptime(
+                        show.time, '%Y-%m-%d %H:%M:%S').date()
+                    if d2 == d1:
+                        flash("Sorry artist is already booked at date!")
+                        return redirect(url_for('create_shows'))
+            newShow = Show(
+                artist_id=artist,
+                venue_id=venue,
+                start_time=start_time
+            )
+
+            db.session.add(newShow)
+            db.session.commit()
+            flash("Show listed successfully!")
+        else:
+            flash(
+                'Date format is not valid. Please use this syntax Year-Month-Day Hour:Minure:Seconds')
+    except:
+        # TODO: on unsuccessful db insert, flash an error instead.
+        print(sys.exc_info())
+        db.session.rollback()
+        flash('An error occurred. Show could not be listed.')
+        # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+    finally:
+        return render_template('pages/home.html')
 
 
 @app.errorhandler(404)
@@ -625,6 +660,19 @@ if not app.debug:
 #----------------------------------------------------------------------------#
 # Launch.
 #----------------------------------------------------------------------------#
+
+
+def isDate(arg):
+    isCorrect = False
+    try:
+        datetime.strftime(arg, '%Y-%m-%d %H:%M:%S')
+        isCorrect = True
+    except:
+        isCorrect = False
+        print("Arguement is not a date")
+    finally:
+        return isCorrect
+
 
 # Default port:
 if __name__ == '__main__':
